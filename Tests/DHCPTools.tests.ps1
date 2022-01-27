d1a765700a6ef968c06a81b89b1b0bd09554374c
Rich Carpenter <artfulbodger80@gmail.com>
2022-01-27 19:09:40 +0000

    tests: Add github action for PSScriptAnalyser

--- Tests/DHCPTools.tests.ps1
+++ Tests/DHCPTools.tests.ps1
@@ -1,10 +1,7 @@
-<<<<<<< Updated upstream
-$moduleRoot = Resolve-Path "$PSScriptRoot\.."
-=======
 Write-Host "$PSScriptRoot"
 $projectRoot = Resolve-Path "$PSScriptRoot\.."
 $moduleRoot = Split-Path (Resolve-Path "$projectRoot\*\*.psm1")
->>>>>>> Stashed changes
+
 $moduleName = Split-Path $moduleRoot -Leaf
 
 Describe "General project validation: $moduleName" {
@@ -12,7 +9,7 @@ Describe "General project validation: $moduleName" {
     $scripts = Get-ChildItem $moduleRoot -Include *.ps1, *.psm1, *.psd1 -Recurse
 
     # TestCases are splatted to the script so we need hashtables
-    $testCase = $scripts | Foreach-Object {@{file = $_}}         
+    $testCase = $scripts | Foreach-Object { @{file = $_ } }
     It "Script <file> should be valid powershell" -TestCases $testCase {
         param($file)
 
@@ -23,11 +20,9 @@ Describe "General project validation: $moduleName" {
         $null = [System.Management.Automation.PSParser]::Tokenize($contents, [ref]$errors)
         $errors.Count | Should Be 0
     }
-<<<<<<< Updated upstream
-=======
 
     It "Module '$moduleName' can import cleanly" {
-        {Import-Module (Join-Path $moduleRoot "$moduleName.psm1") -force } | Should Not Throw
+        { Import-Module (Join-Path $moduleRoot "$moduleName.psm1") -force } | Should Not Throw
     }
 }
 
@@ -36,24 +31,24 @@ foreach ($Manifest in $ManifestPath) {
     $ModuleInfo = Import-Module -Name $Manifest -Force -PassThru
 
     $PS1FileNames = Get-ChildItem -Path "$($ModuleInfo.ModuleBase)\*.ps1" -Exclude *tests.ps1, *profile.ps1 |
-                    Select-Object -ExpandProperty BaseName
+    Select-Object -ExpandProperty BaseName
 
     $ExportedFunctions = Get-Command -Module $ModuleInfo.Name |
-                         Select-Object -ExpandProperty Name
-                         
+    Select-Object -ExpandProperty Name
+
     $manifestfile = Get-Item $manifest
-    
+
     $allfileslist = Get-ChildItem -Path "$($ModuleInfo.ModuleBase)\*.*" -Exclude "$($ModuleInfo.Name).psd1", "$($ModuleInfo.Name).psm1" | Select-Object -ExpandProperty Fullname
 
-  
-  Describe "FunctionsToExport for PowerShell module '$($ModuleInfo.Name)'" {
-  
+
+    Describe "FunctionsToExport for PowerShell module '$($ModuleInfo.Name)'" {
+
         It 'Contains a module in the correct folder name' {
-          $manifestfile.BaseName | Should Be $manifestfile.Directory.Name
+            $manifestfile.BaseName | Should Be $manifestfile.Directory.Name
         }
-        
+
         It 'Contains a root module with the same name as the module' {
-          $ModuleInfo.RootModule | Should Be $manifestfile.BaseName
+            $ModuleInfo.RootModule | Should Be $manifestfile.BaseName
         }
 
         It 'Exports one function in the module manifest per PS1 file' {
@@ -80,5 +75,4 @@ foreach ($Manifest in $ManifestPath) {
         }
     }
 
->>>>>>> Stashed changes
 }
